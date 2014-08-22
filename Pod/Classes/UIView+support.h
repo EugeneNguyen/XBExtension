fe4d045a5f918bb82fb13a80266f1eac841ff3cc
Eugene Nguyen <xuanbinh91@gmail.com>
2014-08-22 17:12:55 +0700

    release 0.0.1.13

---
+++ Pod/Classes/UIView+support.h
@@ -0,0 +1,16 @@
+//
+//  UIView+support.h
+//  Pods
+//
+//  Created by Binh Nguyen Xuan on 8/22/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (support)
+
+- (void)showHUD:(NSString *)string;
+- (void)hideHUD;
+
+@end
