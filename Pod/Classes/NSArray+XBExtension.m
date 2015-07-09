9dff4710eeda9d3e672190052ce7a5a35b4dbfd4
eugenenguyen <xuanbinh91@gmail.com>
2015-07-09 15:04:50 +0700

    Add order function

--- Pod/Classes/NSArray+XBExtension.m
+++ Pod/Classes/NSArray+XBExtension.m
@@ -23,4 +23,30 @@
     return [NSArray arrayWithContentsOfFile:path];
 }
 
+- (NSArray *)arrayOrderedByString:(NSString *)orderField accending:(BOOL)accending
+{
+    return [self sortedArrayUsingComparator:^NSComparisonResult(id  __nonnull obj1, id  __nonnull obj2) {
+        NSString *s1 = [obj1 objectForKey:orderField];
+        NSString *s2 = [obj2 objectForKey:orderField];
+        return [s1 compare:s2];
+    }];
+}
+
+- (NSArray *)arrayOrderedByNumber:(NSString *)orderField accending:(BOOL)accending
+{
+    return [self sortedArrayUsingComparator:^NSComparisonResult(id  __nonnull obj1, id  __nonnull obj2) {
+        float f1 = [[obj1 objectForKey:orderField] floatValue];
+        float f2 = [[obj2 objectForKey:orderField] floatValue];
+        if (f1 > f2)
+        {
+            return NSOrderedDescending;
+        }
+        if (f1 < f2)
+        {
+            return NSOrderedAscending;
+        }
+        return NSOrderedSame;
+    }];
+}
+
 @end
